6bc0d359d8ebd219da7a10b17ad4d49f62d45b34
Raiffep <raiffe.pontes@gmail.com>
2020-07-04 14:20:55 -0300

    Desafio Terminado

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,23 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    let total = 0;
 
-    return formatValue(0);
+    products.forEach(p => {
+      total += p.price * p.quantity;
+    });
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    let total = 0;
 
-    return 0;
+    products.forEach(p => {
+      total += p.quantity;
+    });
+
+    return total;
   }, [products]);
 
   return (
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,37 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    /* const newTotal = products.reduce((accumulator, product) => {
+      const subTotal = product.price * product.quantity;
 
-    return formatValue(0);
+      return accumulator + subTotal;
+    }, 0); */
+
+    let total = 0;
+
+    products.forEach(p => {
+      total += p.price * p.quantity;
+    });
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    let total = 0;
 
-    return 0;
+    products.forEach(p => {
+      total += p.quantity;
+    });
+
+    return total;
   }, [products]);
 
   return (
